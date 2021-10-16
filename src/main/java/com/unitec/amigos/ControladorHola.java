c4b8ee5d19534d28d9d9f6c7d79b7a5fc2e2b24f
Mariana <tellezresendizmariana@gmail.com>
2021-10-15 19:00:16 -0500

    Corrigiendo repositorio

---
+++ src/main/java/com/unitec/amigos/ControladorHola.java
@@ -0,0 +1,15 @@
+package com.unitec.amigos;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ControladorHola {
+    //Este primer recurso es tu Hola Mundo de un servicio REST que usa el metodo GET
+    @GetMapping("/hola")
+    public String saludar(){
+        return "Hola desde mi primer servicio REST";
+    }
+}
